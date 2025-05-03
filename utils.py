dd6e80e1bc15f7f5c696750e82ffd14ecc4274f8
Anas <anasbinwaquar@gmail.com>
2025-05-03 19:45:22 +0500

    Revert "Added pagination for soundboard buttons incase sounds exceed 25"
    This reverts commit f9baf21d1b33f5417601b09c14e526634b389ec3.

--- utils.py
+++ utils.py
@@ -7,60 +7,19 @@ from fetch_data_url import fetch_data_url
 from constants import AVAILABLE_SOUNDS, SOUNDS_FOLDER
 import os
 
-from discord.ui import Button, View
-
-MAX_BUTTONS = 25  # Discord limit
-
-
-class PageButton(Button):
-    def __init__(self, label, page, total_pages, callback_func):
-        super().__init__(label=label, style=discord.ButtonStyle.secondary)
-        self.page = page
-        self.total_pages = total_pages
-        self.callback_func = callback_func
-
-    async def callback(self, interaction):
-        await self.callback_func(interaction, self.page)
-
-
-def create_sound_list_view(page, total_pages, callback_func):
-    view = View(timeout=60 * 60 * 24)
-    sound_names = list(AVAILABLE_SOUNDS.keys())
-    start = page * MAX_BUTTONS
-    end = start + MAX_BUTTONS
-    for sound_name in sound_names[start:end]:
+def create_sound_list_view():
+    view = View(timeout=60*60*24)
+    for sound_name in AVAILABLE_SOUNDS:
         view.add_item(SoundButtonFactory.create(sound_name))
-
-    # Add pagination buttons if needed
-    if total_pages > 1:
-        if page > 0:
-            view.add_item(PageButton('⬅️ Previous', page - 1, total_pages, callback_func))
-        if page < total_pages - 1:
-            view.add_item(PageButton('➡️ Next', page + 1, total_pages, callback_func))
     return view
 
-
 async def send_sound_list_message(message):
-    total_pages = (len(AVAILABLE_SOUNDS) + MAX_BUTTONS - 1) // MAX_BUTTONS
-
-    async def page_callback(interaction, page):
-        view = create_sound_list_view(page, total_pages, page_callback)
-        await interaction.response.edit_message(content="Click a sound to play:", view=view)
-
-    view = create_sound_list_view(0, total_pages, page_callback)
+    view = create_sound_list_view()
     await message.channel.send("Click a sound to play:", view=view)
 
-
 async def send_sound_list_interaction(interaction):
-    total_pages = (len(AVAILABLE_SOUNDS) + MAX_BUTTONS - 1) // MAX_BUTTONS
-
-    async def page_callback(interaction, page):
-        view = create_sound_list_view(page, total_pages, page_callback)
-        await interaction.response.edit_message(content="Click a sound to play:", view=view)
-
-    view = create_sound_list_view(0, total_pages, page_callback)
+    view = create_sound_list_view()
     await interaction.followup.send("Click a sound to play:", view=view)
-
 
 async def play_sound(message, sound_name, sound_path):
     vc = message.guild.voice_client
@@ -79,7 +38,6 @@ async def stop_sound(message):
         await message.channel.send('Stopped the sound.')
     else:
         await message.channel.send('Nothing is playing.')
-
 
 async def add_sound_to_board(message, name, url):
     try:
@@ -102,7 +60,6 @@ async def add_sound_to_board(message, name, url):
         await message.channel.send(f'Failed to download sound: {e}')
         return False
 
-
 async def handle_play_command_with_name(message, sound_name):
     if message.author.voice:
         channel = message.author.voice.channel
@@ -121,7 +78,6 @@ async def handle_play_command_with_name(message, sound_name):
 
     else:
         await message.channel.send('You are not in a voice channel!')
-
 
 async def handle_removesound_command(message):
     parts = message.content.split()
